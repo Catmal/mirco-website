3bdb5c894274b45f5b30ca329cb3adb29d9174e0
Mirco <hi@mirco.space>
2020-12-01 23:50:03 +0800

    dot env

--- src/boot/firebase.js
+++ src/boot/firebase.js
@@ -2,13 +2,13 @@ import firebase from "firebase/app";
 import "firebase/firestore";
 import "firebase/auth"; // for authentication
 const config = {
-	apiKey: "AIzaSyAWVtryIFYc7vIQ51cIgRK1X64f5fIG7sY",
+	apiKey: process.env.VUE_APP_API_KEY,
 	authDomain: "mirco-website.firebaseapp.com",
 	databaseURL: "https://mirco-website.firebaseio.com",
 	projectId: "mirco-website",
-	storageBucket: "mirco-website.appspot.com",
-	messagingSenderId: "150510429609",
-	appId: "1:150510429609:web:a549c722e38c7e0472d0b8"
+	storageBucket: process.env.VUE_APP_STORAGE_BUCKET,
+	messagingSenderId: process.env.VUE_APP_MESSAGING_SENDER_ID,
+	appId: process.env.VUE_APP_EMAIL
 };
 
 firebase.initializeApp(config);
